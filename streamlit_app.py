45fb6056e7c4e1f8fd062cceacc881f0a6d805e2
ab301 <alberthyw@gmail.com>
2025-09-03 17:32:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,184 @@
+# miscTest.py
 import streamlit as st
 
-st.title('🎈 App Name')
+import datetime
+import time
+from datetime import time
 
-st.write('Hello world!')
+# ----------------------------------------------------------------
+st.title("Test basic elements")
+
+st.write("multiselect")
+options = st.multiselect(
+    "What are your favorite cat names?",
+    ["Jellybeans", "Fish Biscuit", "Madam President"],
+    max_selections=5,
+    accept_new_options=True,
+)
+
+st.write("You selected:", options)
+
+# ----------------------------------------------------------------
+
+st.write("------------------------------------------------------")
+color = st.select_slider(
+    "Select a color of the rainbow",
+    options=[
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "indigo",
+        "violet",
+    ],
+)
+st.write("My favorite color is", color)
+
+st.write("------------------------------------------------------")
+
+on = st.toggle("Activate feature")
+
+if on:
+    st.write("Feature activated!")
+    
+st.write("------------------------------------------------------")
+
+# example of a range slider:
+values = st.slider("Select a range of values", 0.0, 100.0, (25.0, 75.0))
+st.write("Values:", values)
+
+
+st.write("------------------------------------------------------")
+# range time slider:
+
+appointment = st.slider(
+    "Schedule your appointment:", value=(time(11, 30), time(12, 45))
+)
+st.write("You're scheduled for:", appointment)
+
+
+st.write("=====================================================")
+
+d = st.date_input("When's your birthday", datetime.date(2019, 7, 6))
+st.write("Your birthday is:", d)
+
+st.write("=====================================================")
+
+
+import pandas as pd
+from numpy.random import default_rng as rng
+
+df = pd.DataFrame(
+    rng(0).standard_normal((10, 20)), columns=("col %d" % i for i in range(20))
+)
+
+st.dataframe(df.style.highlight_max(axis=0))
+
+st.write("=====================================================")
+
+import pandas as pd
+
+df = pd.DataFrame(
+    [
+        {"command": "st.selectbox", "rating": 4, "is_widget": True},
+        {"command": "st.balloons", "rating": 5, "is_widget": False},
+        {"command": "st.time_input", "rating": 3, "is_widget": True},
+    ]
+)
+edited_df = st.data_editor(df)
+
+favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
+st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
+
+st.write("=====================================================")
+
+import pandas as pd
+
+data_df = pd.DataFrame(
+    {
+        "widgets": ["st.selectbox", "st.number_input", "st.text_area", "st.button"],
+    }
+)
+
+st.data_editor(
+    data_df,
+    column_config={
+        "widgets": st.column_config.Column(
+            "Streamlit Widgets",
+            help="Streamlit **widget** commands 🎈",
+            width="medium",
+            required=True,
+        )
+    },
+    hide_index=True,
+    num_rows="dynamic",
+)
+
+
+
+st.write("=====================================================")
+st.write("LinkColumn")
+
+import pandas as pd
+
+data_df = pd.DataFrame(
+    {
+        "apps": [
+            "https://roadmap.streamlit.app",
+            "https://extras.streamlit.app",
+            "https://issues.streamlit.app",
+            "https://30days.streamlit.app",
+        ],
+        "creator": [
+            "https://github.com/streamlit",
+            "https://github.com/arnaudmiribel",
+            "https://github.com/streamlit",
+            "https://github.com/streamlit",
+        ],
+    }
+)
+
+st.data_editor(
+    data_df,
+    column_config={
+        "apps": st.column_config.LinkColumn(
+            "Trending apps",
+            help="The top trending Streamlit apps",
+            validate=r"^https://[a-z]+\.streamlit\.app$",
+            max_chars=100,
+            display_text=r"https://(.*?)\.streamlit\.app"
+        ),
+        "creator": st.column_config.LinkColumn(
+            "App Creator", display_text="Open profile"
+        ),
+    },
+    hide_index=True,
+)
+
+st.write("=====================================================")
+st.write("ImageColumn")
+
+import pandas as pd
+
+data_df = pd.DataFrame(
+    {
+        "apps": [
+            "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
+            "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/ef9a7627-13f2-47e5-8f65-3f69bb38a5c2/Home_Page.png",
+            "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/31b99099-8eae-4ff8-aa89-042895ed3843/Home_Page.png",
+            "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/6a399b09-241e-4ae7-a31f-7640dc1d181e/Home_Page.png",
+        ],
+    }
+)
+
+st.data_editor(
+    data_df,
+    column_config={
+        "apps": st.column_config.ImageColumn(
+            "Preview Image", help="Streamlit app preview screenshots"
+        )
+    },
+    hide_index=True,
+)
+
